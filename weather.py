88369f445219ef3cad92d2c2e31c7c441148a486
dianaluna01 <72881310+dianaluna01@users.noreply.github.com>
2021-10-05 01:16:33 -0400

    Update weather.py

--- weather.py
+++ weather.py
@@ -8,19 +8,19 @@ def weatherDashboard():
     return render_template('weather.html') 
 
 @app.route('/results', methods=['POST'])
-def displayInfo():
+def render_results():
     zip_code = request.form['zipCode']
 
     api_key = "34a7ee6dda364f4215900946c63d9153"
-    data = info(zip_code, api_key)
+    data = get_weather_results(zip_code, api_key)
     temp = "{0:.2f}".format(data["main"]["temp"])
     feels_like = "{0:.2f}".format(data["main"]["feels_like"])
     weather = data["weather"][0]["main"]
-    place = data["name"]
+    location = data["name"]
 
-    return render_template('results.html', place=place, temp=temp, feels_like=feels_like, weather=weather)
+    return render_template('results.html', location=location, temp=temp, feels_like=feels_like, weather=weather)
 
-def info(zip_code, api_key):
+def get_weather_results(zip_code, api_key):
     #removed {country code} because if not specified, it only defaults to USA codes
     api_url = "http://api.openweathermap.org/data/2.5/weather?zip={}&units=imperial&appid={}".format(zip_code, api_key)
     
